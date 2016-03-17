9325bef232158130c522d80feb390411f5af7850
Dmytro Shaforostov <dshaf7@gmail.com>
2016-03-17 14:35:00 +0200

    intersection mode refactored

--- ball.js
+++ ball.js
@@ -11,10 +11,10 @@ var BALL = {
 		FLAGS.movingtop = BALL.currentPoint[1] != to[1];
 		FLAGS.movingleft = BALL.currentPoint[0] != to[0];
 		var distance = Math.sqrt( Math.pow( to[0] - BALL.currentPoint[0] , 2 ) + Math.pow( to[1] - BALL.currentPoint[1] , 2 ) );
-		var distance2 = Math.abs( to[0] - BALL.currentPoint[0] ) + Math.abs( to[1] - BALL.currentPoint[1] );
+		//var distance2 = Math.abs( to[0] - BALL.currentPoint[0] ) + Math.abs( to[1] - BALL.currentPoint[1] );
 		
-		var travelTime = 1000 * distance;
-		console.log(distance / distance2)
+		var travelTime = 100 * distance;
+		//console.log(distance / distance2)
 
 		this.element.setAttribute("style","left:"+(to[0]*100)+"%;top:"+(to[1]*100)+"%;transition-duration:"+travelTime+"ms;")
 	}
@@ -43,12 +43,9 @@ BALL.element.addEventListener('transitionend', function(e) {
 
 
 	var intersection = barrier.check(BALL.nextPoint, BALL.destinationPoint);
-	
+
 	if (intersection) {
-		//console.log("intersection", intersection)
-		//console.log(BALL.nextPoint, BALL.destinationPoint)
 		BALL.destinationPoint = [intersection[0], intersection[1]];
-		//alert("intersection!")
 	};
 });
 
@@ -63,7 +60,7 @@ function makeB() {BALL.destinationPoint = getRandomDestinationPoint();
 function getRandomDestinationPoint () {
 		var projection = Math.random() * 4;
 		// integer part of projection: id of screen side; fractional part: position on that side
-		return projection < 2 ? ( projection  < 1 ? [projection, 0] : [0, projection - 1] ) : ( projection < 3 ? [projection - 2, 1] : [1, projection - 3] )
+		return projection < 2 ? ( projection  < 1 ? [0, projection] : [projection - 1, 0] ) : ( projection < 3 ? [0, projection - 2] : [projection - 3, 1] )
 }
 
 function Barrier(element) {
@@ -76,64 +73,6 @@ function Barrier(element) {
 	this.ySegment = [(element.offsetTop - radius) / parentHeight, (element.offsetTop + element.offsetHeight + radius) / parentHeight]
 
 
-	this.check = function (p1,p2) {
-		var k = (p2[1] - p1[1])/(p2[0] - p1[0]), // k for y=; for x= use 1/k
-			coordinate;
-
-		if ( p1[1] < this.ySegment[0] && p2[1] > this.ySegment[0] ) {
-			coordinate = this.__getIntersectionOf2Straights(this.ySegment[0], 1/k, p1[1], p1[0]);
-			if ( this.__isEnteringIntoaSegment(this.xSegment, coordinate) ) return [coordinate , this.ySegment[0]];
-			
-			if (coordinate < this.xSegment[0]) {
-				coordinate = this.__getIntersectionOf2Straights(this.xSegment[0], k, p1[0], p1[1]);
-				if ( this.__isEnteringIntoaSegment(this.ySegment, coordinate) ) return [this.xSegment[0], coordinate];
-			} else {
-				coordinate = this.__getIntersectionOf2Straights(this.xSegment[1], k, p1[0], p1[1]);
-				if ( this.__isEnteringIntoaSegment(this.ySegment, coordinate) ) return [this.xSegment[1], coordinate];
-			}
-		};
-
-		if ( p1[1] > this.ySegment[1] && p2[1] < this.ySegment[1] ) {
-			coordinate = this.__getIntersectionOf2Straights(this.ySegment[1], 1/k, p1[1], p1[0]);
-			if ( this.__isEnteringIntoaSegment(this.xSegment, coordinate) ) return [coordinate , this.ySegment[1]];
-			
-			if (coordinate < this.xSegment[0]) {
-				coordinate = this.__getIntersectionOf2Straights(this.xSegment[0], k, p1[0], p1[1]);
-				if ( this.__isEnteringIntoaSegment(this.ySegment, coordinate) ) return [this.xSegment[0], coordinate];
-			} else {
-				coordinate = this.__getIntersectionOf2Straights(this.xSegment[1], k, p1[0], p1[1]);
-				if ( this.__isEnteringIntoaSegment(this.ySegment, coordinate) ) return [this.xSegment[1], coordinate];
-			}
-		};
-
-		if ( p1[0] < this.xSegment[0] && p2[0] > this.xSegment[1] ) {
-			coordinate = this.__getIntersectionOf2Straights(this.xSegment[0], k, p1[0], p1[1]);
-			if ( this.__isEnteringIntoaSegment(this.ySegment, coordinate) ) return [this.xSegment[0], coordinate];
-			
-			if (coordinate < this.xSegment[0]) {
-				coordinate = this.__getIntersectionOf2Straights(this.ySegment[0], 1/k, p1[1], p1[0]);
-				if ( this.__isEnteringIntoaSegment(this.xSegment, coordinate) ) return [coordinate , this.ySegment[0]];
-			} else {
-				coordinate = this.__getIntersectionOf2Straights(this.ySegment[1], 1/k, p1[1], p1[0]);
-				if ( this.__isEnteringIntoaSegment(this.xSegment, coordinate) ) return [coordinate , this.ySegment[1]];
-			}
-		};
-
-		if ( p1[0] > this.xSegment[1] && p2[0] < this.xSegment[0] ) {
-			coordinate = this.__getIntersectionOf2Straights(this.xSegment[1], k, p1[0], p1[1]);
-			if ( this.__isEnteringIntoaSegment(this.ySegment, coordinate) ) return [this.xSegment[1], coordinate];
-			
-			if (coordinate < this.xSegment[0]) {
-				coordinate = this.__getIntersectionOf2Straights(this.ySegment[0], 1/k, p1[1], p1[0]);
-				if ( this.__isEnteringIntoaSegment(this.xSegment, coordinate) ) return [coordinate , this.ySegment[0]];
-			} else {
-				coordinate = this.__getIntersectionOf2Straights(this.ySegment[1], 1/k, p1[1], p1[0]);
-				if ( this.__isEnteringIntoaSegment(this.xSegment, coordinate) ) return [coordinate , this.ySegment[1]];
-			}
-		};
-
-		return false
-	}
 
 
 
@@ -145,22 +84,34 @@ function Barrier(element) {
 
 
 	this.__getIntersectionOf2Straights = function (y, k, f1, x1) {
-		return y * k - f1 * k + x1
+		return (y - f1) * k + x1
 	}
 
 
+	this.check = function (p1,p2) {
+		var k = (p2[1] - p1[1])/(p2[0] - p1[0]), // k for y=; for x= use 1/k
+			coordinate, intersectionDirection;
+
+		aboveMap = this.__pointAboveWhat(p1);
+
+		for (var i = 0; i < 4; i++) {
+			if (!aboveMap[i]) continue;
+			intersectionDirection = i%2 ? [this.ySegment[ +(i - 2 >= 0) ], 1/k, p1[1], p1[0], "x"] : [this.xSegment[ +(i - 2 >= 0) ], k, p1[0], p1[1], "y"] ;
+			coordinate = this.__getIntersectionOf2Straights(intersectionDirection[0], intersectionDirection[1], intersectionDirection[2], intersectionDirection[3]);
+			if ( this.__isEnteringIntoaSegment(this[ intersectionDirection[4]+"Segment" ], coordinate) ) return intersectionDirection[4] === "x" ? [coordinate, intersectionDirection[0], i] : [ intersectionDirection[0], coordinate, i];
+		};
+
+		}
 
 
-
-
-
-
-
-
-
-
-
-
+	this.__pointAboveWhat = function (p) {
+		return [
+			 p[0] < this.xSegment[0],
+			 p[1] < this.ySegment[0],
+			 p[0] > this.xSegment[1],
+			 p[1] > this.ySegment[1]
+		 ]
+	}
 
 
 
